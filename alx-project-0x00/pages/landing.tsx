87b50d3f6c6adcddcd62f071a5782545b1ace6d3
Blessed Sode <emi.square23@gmail.com>
2025-11-10 20:13:41 +0100

    task 6

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,12 +7,14 @@ const Landing: React.FC =  () => {
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
       <Card />
-      <Card />
       <div className="text-sm rounded-sm">
         <Button title="Get Started" />
       </div>
       <div className="text-md rounded-md">
         <Button title="Learn More" />
+      </div>
+      <div className="text-lg rounded-lg">
+        <Button title="Contact Us" />
       </div>
       <div className="text-lg rounded-full">
         <Button title="Contact Us" />
