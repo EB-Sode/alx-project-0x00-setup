bd8dfcfe9070240c0946e4c68b4b69acf3f20f1b
Blessed Sode <emi.square23@gmail.com>
2025-11-10 20:11:52 +0100

    task 6

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -2,9 +2,7 @@
 const Button: React.FC<{ title: string }> = ({ title }) => {
     return (
         <button className="bg-red-500 text text-white px-4 py-2 rounded-md hover:bg-red-600 transition-colors">
-            <p className="text-sm rounded-sm">{title}</p>
-            <p className="text-md rounded-md">{title}</p>
-            <p className="text-lg rounded-full">{title}</p>
+            {title}
         </button>
     )
 }
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -8,7 +8,15 @@ const Landing: React.FC =  () => {
       <Card />
       <Card />
       <Card />
-      <Button title="Get Started" />
+      <div className="text-sm rounded-sm">
+        <Button title="Get Started" />
+      </div>
+      <div className="text-md rounded-md">
+        <Button title="Learn More" />
+      </div>
+      <div className="text-lg rounded-full">
+        <Button title="Contact Us" />
+      </div>
 
     </div>
   )
